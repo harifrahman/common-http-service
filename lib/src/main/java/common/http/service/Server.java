ae333975c6e0d62a0be24e9e2565b3ccb1e5591d
Arif Hakim <arif.hakim@gopay.co.id>
2024-05-12 23:39:06 +0700

    feat: set each route for modify heroes

--- lib/src/main/java/common/http/service/Server.java
+++ lib/src/main/java/common/http/service/Server.java
@@ -8,8 +8,52 @@ public class Server {
         initExceptionHandler((e) -> System.out.println("Something wruung.." + e.getMessage()));
         get("/ping", (req, res) -> "pong");
 
-        get("/heroes", (req, res) -> {
-            return "{\"lion\": []}";
+        path("/heroes", () -> {
+            get("", (req, res) -> {
+                return "{\n" +
+                        "  \"success\": true,\n" +
+                        "  \"data\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 1,\n" +
+                        "      \"name\": \"lion\",\n" +
+                        "      \"faction\": \"dire\",\n" +
+                        "      \"attackType\": \"ranged\",\n" +
+                        "      \"primaryAttribute\": \"int\"\n" +
+                        "    },\n" +
+                        "    {\n" +
+                        "      \"id\": 2,\n" +
+                        "      \"name\": \"lion\",\n" +
+                        "      \"faction\": \"dire\",\n" +
+                        "      \"attackType\": \"ranged\",\n" +
+                        "      \"primaryAttribute\": \"int\"\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}";
+            });
+
+            get("/:id", (req, res) -> {
+                return """
+                        {
+                          "success": true,
+                          "data": [
+                            {
+                              "id": 1,
+                              "name": "lion",
+                              "faction": "dire",
+                              "attackType": "ranged",
+                              "primaryAttribute": "int"
+                            }
+                          ]
+                        }""";
+            });
+
+            post("", (req, res) -> {
+                return "this is post heroes to add new heroes";
+            });
+
+            delete("/:id",  (req, res) -> {
+                return "this is post heroes to add new heroes";
+            });
         });
     }
 }
