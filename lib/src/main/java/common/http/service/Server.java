4263342cd2a8311622bea711336f12f0cba79554
Arif Hakim <arif.hakim@gopay.co.id>
2024-05-13 00:45:57 +0700

    feat: install gson and create json as response transformer
    gson will help us to transform class object from response to be json format

--- lib/src/main/java/common/http/service/Server.java
+++ lib/src/main/java/common/http/service/Server.java
@@ -1,6 +1,7 @@
 package common.http.service;
 
 import common.http.service.controllers.HeroesController;
+import common.http.service.controllers.JsonTransformer;
 import common.http.service.services.HeroesService;
 
 import java.util.Collections;
@@ -11,15 +12,16 @@ public class Server {
 
     public static void main(String[] args) {
         HeroesController heroesController = new HeroesController(new HeroesService(Collections.emptyList()));
+        JsonTransformer jsonTransformer = new JsonTransformer();
         port(8080);
 
         initExceptionHandler((e) -> System.out.println("Something wruung.." + e.getMessage()));
         get("/ping", (req, res) -> "pong");
 
         path("/heroes", () -> {
-            get("", heroesController::get);
+            get("", heroesController::get, jsonTransformer);
 
-            get("/:id", heroesController::getByIndex);
+            get("/:id", heroesController::getByIndex, jsonTransformer);
 
             post("", (req, res) -> {
                 return "this is post heroes to add new heroes";
