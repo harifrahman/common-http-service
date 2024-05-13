04b37ebfde5400717e2fa030e79d530f78386d32
Arif Hakim <arif.hakim@gopay.co.id>
2024-05-13 09:34:48 +0700

    test: add integration test for getByIndex and refactor to reduce redundant

--- lib/src/test/java/common/http/service/integration/HeroesIntegrationTest.java
+++ lib/src/test/java/common/http/service/integration/HeroesIntegrationTest.java
@@ -4,30 +4,52 @@ import common.http.service.Server;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import spark.Spark;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HeroesIntegrationTest {
+    @BeforeAll
+    static void beginServing() {
+        Spark.port(8080); // this must same with port we select to run our app
+        Server.main(null);
+    }
+
+    @AfterAll
+    static void stopServing() {
+        Spark.stop();
+    }
 
     @Test
     public void testGetHeroesEndpoint() throws Exception {
         String expectedResponseBody = "[{\"id\":1,\"name\":\"lion\",\"faction\":\"dire\",\"attackType\":\"ranged\",\"primaryAttribute\":\"int\"},{\"id\":2,\"name\":\"medusa\",\"faction\":\"dire\",\"attackType\":\"ranged\",\"primaryAttribute\":\"agi\"}]";
 
-        Spark.port(8080); // this must same with port we select to run our app
-        Server.main(null);
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url("http://localhost:8080/heroes")
-                .build();
-        Response response = client.newCall(request).execute();
+        Response response = executeRequestCall("http://localhost:8080/heroes");
 
         assertEquals(200, response.code());
         assertEquals(expectedResponseBody, response.body().string());
+    }
 
-        Spark.stop();
+    @Test
+    public void testGetHeroesByIdEndpoint() throws Exception {
+        String expectedResponseBody = "{\"id\":1,\"name\":\"lion\",\"faction\":\"dire\",\"attackType\":\"ranged\",\"primaryAttribute\":\"int\"}";
+
+        Response response = executeRequestCall("http://localhost:8080/heroes/1");
+
+        assertEquals(200, response.code());
+        assertEquals(expectedResponseBody, response.body().string());
+    }
+
+    private Response executeRequestCall(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        return client.newCall(request).execute();
     }
 }
